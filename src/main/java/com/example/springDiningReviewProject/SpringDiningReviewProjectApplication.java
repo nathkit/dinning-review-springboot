6bc184558d0531f1db04982a3bd6eb7d79fe966f
nathkit <nathkit101@gmail.com>
2024-12-27 19:24:39 +0700

    feat: project starter

---
+++ src/main/java/com/example/springDiningReviewProject/SpringDiningReviewProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springDiningReviewProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiningReviewProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDiningReviewProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springDiningReviewProject/SpringDiningReviewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springDiningReviewProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiningReviewProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
